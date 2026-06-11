e075552c5eace577e39bebdcf9fb34c99dc790d3
suyoungShin-dev <h2suyoung@naver.com>
2026-06-11 14:11:17 +0900

    update error

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,10 +10,13 @@ export default function Header() {
 
   const isLoginPage = pathname === "/";
 
+  const [isMounted, setIsMounted] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
   const [userName, setUserName] = useState("");
 
   useEffect(() => {
+    setIsMounted(true);
+
     const adminYn = localStorage.getItem("isAdmin");
     const loginUserName = localStorage.getItem("loginUserName");
 
@@ -21,46 +24,23 @@ export default function Header() {
     setUserName(loginUserName ?? "");
   }, [pathname]);
 
-  const getTitle = () => {
-    if (pathname.includes("/history-trip/new")) return "여행 추가하기";
-    if (pathname.includes("/trip")) return "저장된 여행정보";
-    if (pathname.includes("/history-trip")) return "여행";
-    if (pathname.includes("/board")) return "한줄대화";
-    if (pathname.includes("/vote")) return "투표";
-    if (pathname.includes("/admin")) return "관리";
-    if (pathname.includes("/notice")) return "공지사항";
-
-    return "";
-  };
-
   const handleHomeClick = () => {
     const isLogin = localStorage.getItem("isLogin");
 
-    if (isLogin === "Y") {
-      router.push("/main");
-    } else {
-      router.push("/");
-    }
+    if (isLogin === "Y") router.push("/main");
+    else router.push("/");
   };
 
   return (
-
-    <header className="relative h-[90]">
+    <header className="relative h-[90px]">
       <h1
         onClick={handleHomeClick}
-        className="
-                    absolute left-[32px] top-[53px]
-                    flex h-[28px] w-[79px]
-                    cursor-pointer items-center justify-center
-                    rounded-[25px]
-                    bg-[#E1EEFD]
-                    text-[13px] font-normal leading-none
-                    text-[#1C70D7]"
+        className="absolute left-[32px] top-[53px] flex h-[28px] w-[79px] cursor-pointer items-center justify-center rounded-[25px] bg-[#E1EEFD] text-[13px] font-normal leading-none text-[#1C70D7]"
       >
         11-1=0 ✈️
       </h1>
 
-      {!isLoginPage && (
+      {isMounted && !isLoginPage && (
         <div className="absolute right-[20px] top-[53px] flex items-center gap-2">
           {userName && (
             <span className="flex items-center gap-[4px] text-[12px] font-normal leading-[20px] text-[#000000]">
@@ -79,7 +59,6 @@ export default function Header() {
           )}
         </div>
       )}
-
     </header>
   );
 }
