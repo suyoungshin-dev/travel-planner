a9b1fbe9457f052fcc9d03dbcce2f2d18d7c3087
suyoungShin-dev <h2suyoung@naver.com>
2026-06-11 15:10:12 +0900

    update main page2

--- app/components/common/PageLayout.tsx
+++ app/components/common/PageLayout.tsx
@@ -6,7 +6,8 @@ export default function PageLayout({
   children,
 }: PageLayoutProps) {
   return (
-    <main className="min-h-screen bg-white">
+    //<main className="min-h-screen bg-white">
+    <main className="min-h-[100dvh] bg-white">
       
       {/* 실제 컨텐츠 영역 */}
       <div
